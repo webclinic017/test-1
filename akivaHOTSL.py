3018a326579085663fc039e0f9c09de5fc18d0cb
akivacz <89083106+akivacz@users.noreply.github.com>
2021-08-25 22:22:49 +0200

    Add files via upload

---
+++ akivaHOTSL.py
@@ -0,0 +1,379 @@
+# --- Do not remove these libs ---
+# --- Do not remove these libs ---
+from freqtrade.strategy.interface import IStrategy
+from typing import Dict, List
+from functools import reduce
+from pandas import DataFrame
+# --------------------------------
+import talib.abstract as ta
+import numpy as np
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+import datetime
+from technical.util import resample_to_interval, resampled_merge
+from datetime import datetime, timedelta
+from freqtrade.persistence import Trade
+from freqtrade.strategy import stoploss_from_open, merge_informative_pair, DecimalParameter, IntParameter, CategoricalParameter
+import technical.indicators as ftt
+from freqtrade.exchange import timeframe_to_prev_date
+
+# @Rallipanos
+
+# # Buy hyperspace params:
+# buy_params = {
+#     "base_nb_candles_buy": 14,
+#     "ewo_high": 2.327,
+#     "ewo_high_2": -2.327,
+#     "ewo_low": -20.988,
+#     "low_offset": 0.975,
+#     "low_offset_2": 0.955,
+#     "rsi_buy": 69
+# }
+
+# # Buy hyperspace params:
+# buy_params = {
+#     "base_nb_candles_buy": 18,
+#     "ewo_high": 3.422,
+#     "ewo_high_2": -3.436,
+#     "ewo_low": -8.562,
+#     "low_offset": 0.966,
+#     "low_offset_2": 0.959,
+#     "rsi_buy": 66,
+# }
+
+# # # Sell hyperspace params:
+# # sell_params = {
+# #     "base_nb_candles_sell": 17,
+# #     "high_offset": 0.997,
+# #     "high_offset_2": 1.01,
+# # }
+
+# # Sell hyperspace params:
+# sell_params = {
+#     "base_nb_candles_sell": 7,
+#     "high_offset": 1.014,
+#     "high_offset_2": 0.995,
+# }
+
+# # Buy hyperspace params:
+# buy_params = {
+#     "ewo_high_2": -5.642,
+#     "low_offset_2": 0.951,
+#     "rsi_buy": 54,
+#     "base_nb_candles_buy": 16,  # value loaded from strategy
+#     "ewo_high": 3.422,  # value loaded from strategy
+#     "ewo_low": -8.562,  # value loaded from strategy
+#     "low_offset": 0.966,  # value loaded from strategy
+# }
+
+# # Sell hyperspace params:
+# sell_params = {
+#     "base_nb_candles_sell": 8,
+#     "high_offset_2": 1.002,
+#     "high_offset": 1.014,  # value loaded from strategy
+# }
+
+# Buy hyperspace params:
+buy_params = {
+    "base_nb_candles_buy": 8,
+    "ewo_high": 4.179,
+    "ewo_low": -16.917,
+    "ewo_high_2": -2.609,  # value loaded from strategy
+    "low_offset": 0.986,  # value loaded from strategy
+    "low_offset_2": 0.944,  # value loaded from strategy
+    "rsi_buy": 58,  # value loaded from strategy
+}
+
+# Sell hyperspace params:
+sell_params = {
+    "base_nb_candles_sell": 16,
+    "high_offset": 1.054,
+    "high_offset_2": 1.018,  # value loaded from strategy
+    "high_offset_ema": 1.012,
+    "pHSL": -0.15,  # value loaded from strategy
+    "pPF_1": 0.018,  # value loaded from strategy
+    "pPF_2": 0.08,  # value loaded from strategy
+    "pSL_1": 0.013,  # value loaded from strategy
+    "pSL_2": 0.04,  # value loaded from strategy
+}
+
+
+def EWO(dataframe, ema_length=5, ema2_length=35):
+    df = dataframe.copy()
+    ema1 = ta.EMA(df, timeperiod=ema_length)
+    ema2 = ta.EMA(df, timeperiod=ema2_length)
+    emadif = (ema1 - ema2) / df['low'] * 100
+    return emadif
+
+
+class akivaHOTSL(IStrategy):
+    INTERFACE_VERSION = 2
+
+    # ROI table:
+    minimal_roi = {
+        # "0": 0.283,
+        # "40": 0.086,
+        # "99": 0.036,
+        "0": 10
+    }
+
+    # Stoploss:
+    stoploss = -0.04
+
+    # SMAOffset
+    high_offset_ema = DecimalParameter(0.99, 1.1, default=1.012, load=True, space='sell', optimize=False)
+    base_nb_candles_buy = IntParameter(
+        2, 20, default=buy_params['base_nb_candles_buy'], space='buy', optimize=False)
+    base_nb_candles_sell = IntParameter(
+        10, 40, default=sell_params['base_nb_candles_sell'], space='sell', optimize=False)
+    low_offset = DecimalParameter(
+        0.9, 0.99, default=buy_params['low_offset'], space='buy', optimize=False)
+    low_offset_2 = DecimalParameter(
+        0.9, 0.99, default=buy_params['low_offset_2'], space='buy', optimize=False)
+    high_offset = DecimalParameter(
+        0.95, 1.1, default=sell_params['high_offset'], space='sell', optimize=False)
+    high_offset_2 = DecimalParameter(
+        0.99, 1.5, default=sell_params['high_offset_2'], space='sell', optimize=False)
+
+    # Protection
+    fast_ewo = 50
+    slow_ewo = 200
+    ewo_low = DecimalParameter(-20.0, -8.0,
+                               default=buy_params['ewo_low'], space='buy', optimize=False)
+    ewo_high = DecimalParameter(
+        2.0, 12.0, default=buy_params['ewo_high'], space='buy', optimize=False)
+
+    ewo_high_2 = DecimalParameter(
+        -6.0, 12.0, default=buy_params['ewo_high_2'], space='buy', optimize=False)
+
+    rsi_buy = IntParameter(30, 70, default=buy_params['rsi_buy'], space='buy', optimize=True)
+
+    # Trailing stop:
+    trailing_stop = True
+    trailing_stop_positive = 0.001
+    trailing_stop_positive_offset = 0.018
+    trailing_only_offset_is_reached = True
+
+    # Sell signal
+    use_sell_signal = True
+    sell_profit_only = False
+    sell_profit_offset = 0.001
+    ignore_roi_if_buy_signal = False
+
+    # Optional order time in force.
+    order_time_in_force = {
+        'buy': 'gtc',
+        'sell': 'ioc'
+    }
+
+    # Optimal timeframe for the strategy
+    timeframe = '5m'
+    inf_1h = '1h'
+
+    process_only_new_candles = True
+    startup_candle_count = 200
+    use_custom_stoploss = False
+
+    plot_config = {
+        'main_plot': {
+            'ma_buy': {'color': 'orange'},
+            'ma_sell': {'color': 'orange'},
+        },
+    }
+
+    slippage_protection = {
+        'retries': 3,
+        'max_slippage': -0.02
+    }
+
+    buy_signals = {}
+
+    pHSL = DecimalParameter(-0.200, -0.040, default=-0.15, decimals=3, space='sell', optimize=False, load=True)
+    # profit threshold 1, trigger point, SL_1 is used
+    pPF_1 = DecimalParameter(0.008, 0.020, default=0.018, decimals=3, space='sell', optimize=False, load=True)
+    pSL_1 = DecimalParameter(0.008, 0.020, default=0.013, decimals=3, space='sell', optimize=False, load=True)
+
+    # profit threshold 2, SL_2 is used
+    pPF_2 = DecimalParameter(0.040, 0.100, default=0.080, decimals=3, space='sell', optimize=False, load=True)
+    pSL_2 = DecimalParameter(0.020, 0.070, default=0.040, decimals=3, space='sell', optimize=False, load=True)
+
+    # Custom Trailing Stoploss by Perkmeister
+    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
+                        current_rate: float, current_profit: float, **kwargs) -> float:
+
+        # hard stoploss profit
+        HSL = self.pHSL.value
+        PF_1 = self.pPF_1.value
+        SL_1 = self.pSL_1.value
+        PF_2 = self.pPF_2.value
+        SL_2 = self.pSL_2.value
+
+        # For profits between PF_1 and PF_2 the stoploss (sl_profit) used is linearly interpolated
+        # between the values of SL_1 and SL_2. For all profits above PL_2 the sl_profit value 
+        # rises linearly with current profit, for profits below PF_1 the hard stoploss profit is used.
+
+        if (current_profit > PF_2):
+            sl_profit = SL_2 + (current_profit - PF_2)
+        elif (current_profit > PF_1):
+            sl_profit = SL_1 + ((current_profit - PF_1)*(SL_2 - SL_1)/(PF_2 - PF_1))
+        else:
+            sl_profit = HSL
+        
+        return stoploss_from_open(sl_profit, current_profit)
+
+    def get_ticker_indicator(self):
+        return int(self.timeframe[:-1])
+
+    def custom_sell(self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float,
+                    current_profit: float, **kwargs):
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+        buy_tag = 'empty'
+        if hasattr(trade, 'buy_tag') and trade.buy_tag is not None:
+            buy_tag = trade.buy_tag
+        else:
+            trade_open_date = timeframe_to_prev_date(self.timeframe, trade.open_date_utc)
+            buy_signal = dataframe.loc[dataframe['date'] < trade_open_date]
+            if not buy_signal.empty:
+                buy_signal_candle = buy_signal.iloc[-1]
+                buy_tag = buy_signal_candle['buy_tag'] if buy_signal_candle['buy_tag'] != '' else 'empty'
+        buy_tags = buy_tag.split()
+
+        last_candle = dataframe.iloc[-1].squeeze()
+
+        if (last_candle['close'] > (last_candle['ema_offset_sell'])) :
+            return 'sell signal (' + buy_tag +')'
+
+        return None
+
+    def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
+                           rate: float, time_in_force: str, sell_reason: str,
+                           current_time: datetime, **kwargs) -> bool:
+
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+        last_candle = dataframe.iloc[-1]
+
+        if (last_candle is not None):
+            if (sell_reason in ['sell_signal']):
+                if (last_candle['hma_50']*1.149 > last_candle['ema_100']) and (last_candle['close'] < last_candle['ema_100']*0.951):  # *1.2
+                    return False
+
+        # slippage
+        try:
+            state = self.slippage_protection['__pair_retries']
+        except KeyError:
+            state = self.slippage_protection['__pair_retries'] = {}
+
+        candle = dataframe.iloc[-1].squeeze()
+
+        slippage = (rate / candle['close']) - 1
+        if slippage < self.slippage_protection['max_slippage']:
+            pair_retries = state.get(pair, 0)
+            if pair_retries < self.slippage_protection['retries']:
+                state[pair] = pair_retries + 1
+                return False
+
+        state[pair] = 0
+        current_profit = trade.calc_profit_ratio(rate)
+        if (sell_reason.startswith('sell signal ') and (current_profit > self.pPF_1.value)):
+            # Reject sell signal when trailing stoplosses
+            return False
+        return True
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        # Calculate all ma_buy values
+        for val in self.base_nb_candles_buy.range:
+            dataframe[f'ma_buy_{val}'] = ta.EMA(dataframe, timeperiod=val)
+
+        # Calculate all ma_sell values
+        for val in self.base_nb_candles_sell.range:
+            dataframe[f'ma_sell_{val}'] = ta.EMA(dataframe, timeperiod=val)
+
+        dataframe['hma_50'] = qtpylib.hull_moving_average(dataframe['close'], window=50)
+        dataframe['ema_100'] = ta.EMA(dataframe, timeperiod=100)
+
+        dataframe['sma_9'] = ta.SMA(dataframe, timeperiod=9)
+        # Elliot
+        dataframe['EWO'] = EWO(dataframe, self.fast_ewo, self.slow_ewo)
+
+        # RSI
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+        dataframe['rsi_fast'] = ta.RSI(dataframe, timeperiod=4)
+        dataframe['rsi_slow'] = ta.RSI(dataframe, timeperiod=20)
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        dataframe.loc[
+            (
+                (dataframe['rsi_fast'] < 35) &
+                (dataframe['close'] < (dataframe[f'ma_buy_{self.base_nb_candles_buy.value}'] * self.low_offset.value)) &
+                (dataframe['EWO'] > self.ewo_high.value) &
+                (dataframe['rsi'] < self.rsi_buy.value) &
+                (dataframe['volume'] > 0) &
+                (dataframe['close'] < (
+                    dataframe[f'ma_sell_{self.base_nb_candles_sell.value}'] * self.high_offset.value))
+            ),
+            ['buy', 'buy_tag']] = (1, 'ewo1')
+
+        dataframe.loc[
+            (
+                (dataframe['rsi_fast'] < 35) &
+                (dataframe['close'] < (dataframe[f'ma_buy_{self.base_nb_candles_buy.value}'] * self.low_offset_2.value)) &
+                (dataframe['EWO'] > self.ewo_high_2.value) &
+                (dataframe['rsi'] < self.rsi_buy.value) &
+                (dataframe['volume'] > 0) &
+                (dataframe['close'] < (dataframe[f'ma_sell_{self.base_nb_candles_sell.value}'] * self.high_offset.value)) &
+                (dataframe['rsi'] < 25)
+            ),
+            ['buy', 'buy_tag']] = (1, 'ewo2')
+
+        dataframe.loc[
+            (
+                (dataframe['rsi_fast'] < 35) &
+                (dataframe['close'] < (dataframe[f'ma_buy_{self.base_nb_candles_buy.value}'] * self.low_offset.value)) &
+                (dataframe['EWO'] < self.ewo_low.value) &
+                (dataframe['volume'] > 0) &
+                (dataframe['close'] < (
+                    dataframe[f'ma_sell_{self.base_nb_candles_sell.value}'] * self.high_offset.value))
+            ),
+            ['buy', 'buy_tag']] = (1, 'ewolow')
+
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions = []
+
+        conditions.append(
+            ((dataframe['close'] > dataframe['sma_9']) &
+                (dataframe['close'] > (dataframe[f'ma_sell_{self.base_nb_candles_sell.value}'] * self.high_offset_2.value)) &
+                (dataframe['rsi'] > 50) &
+                (dataframe['volume'] > 0) &
+                (dataframe['rsi_fast'] > dataframe['rsi_slow'])
+             )
+            |
+            (
+                (dataframe['close'] < dataframe['hma_50']) &
+                (dataframe['close'] > (dataframe[f'ma_sell_{self.base_nb_candles_sell.value}'] * self.high_offset.value)) &
+                (dataframe['volume'] > 0) &
+                (dataframe['rsi_fast'] > dataframe['rsi_slow'])
+            )
+
+        )
+
+        dataframe['ema_offset_sell'] = ta.EMA(dataframe, int(self.base_nb_candles_sell.value)) *self.high_offset_ema.value
+
+        if conditions:
+            dataframe.loc[
+                reduce(lambda x, y: x | y, conditions),
+                'sell'
+            ]=1
+
+        return dataframe
+
+def EWO(dataframe, sma1_length=5, sma2_length=35):
+    df = dataframe.copy()
+    sma1 = ta.EMA(df, timeperiod=sma1_length)
+    sma2 = ta.EMA(df, timeperiod=sma2_length)
+    smadif = (sma1 - sma2) / df['close'] * 100
+    return smadif
